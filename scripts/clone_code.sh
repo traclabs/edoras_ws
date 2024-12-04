783231af0581dda4a40d55ceb60dae972494fb09
Ana C. Huaman Quispe <ana@traclabs.com>
2024-12-04 09:51:12 -0600

    Improving clone script to not try to overwrite

--- scripts/clone_code.sh
+++ scripts/clone_code.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
 clone_cfs() {
-  echo "Cloning cFS"
+
+  if [ -d "cFS" ]
+  then
+    echo "* Directory cFS already exists, not cloning"
+    return 1
+  fi
+
+  echo "* Cloning cFS..."
   git clone -b edoras git@github.com:traclabs/cFS.git
   pushd cFS
   git submodule update --init --recursive
   popd
-  echo "Cloning gateway app"
+  echo "* Cloning gateway app..."
   git clone -b master git@github.com:traclabs/gateway_app.git cFS/apps/gateway_app
 }
 
-clone_edoras {
-  echo "Cloning edoras metapackage"
+clone_edoras() {
+
+  if [ -d "edoras" ]
+  then
+    echo "* Directory edoras already exists, not cloning"
+    return 1
+  fi
+
+  echo "* Cloning edoras metapackage... "
   git clone -b master git@github.com:traclabs/edoras edoras
 }
 
